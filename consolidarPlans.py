dce3b4fd22a8c0e26fadc9fa0c6a05f995448fc7
Edyelgue <edyelgue12@gmail.com>
2024-09-10 13:17:15 -0300

    commit inicial

---
+++ consolidarPlans.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+# Diretório onde os arquivos Excel estão localizados
+diretorio = "C:/Users/edyelgue.carneiro/Downloads/gmud"
+
+# Lista para armazenar os DataFrames de cada arquivo
+dataframes = []
+
+# Loop por todos os arquivos no diretório
+for arquivo in os.listdir(diretorio):
+    if arquivo.endswith('.xlsx') or arquivo.endswith('.xls'):
+        # Ler o arquivo Excel e adicionar ao DataFrame
+        caminho_arquivo = os.path.join(diretorio, arquivo)
+        df = pd.read_excel(caminho_arquivo)
+        dataframes.append(df)
+
+# Concatenar todos os DataFrames em um só
+df_unido = pd.concat(dataframes, ignore_index=True)
+
+# Salvar o DataFrame unido em um novo arquivo Excel
+df_unido.to_excel('arquivo_unido.xlsx', index=False)
